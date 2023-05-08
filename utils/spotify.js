a6ad45d158278f503e078035e375507ef93bc5fa
EZenderock <zenderockemmanuel@gmail.com>
2023-05-08 06:01:40 +0100

    Update spotify.js

--- utils/spotify.js
+++ utils/spotify.js
@@ -1,6 +1,5 @@
 // Authorization token that must have been created previously. See : https://developer.spotify.com/documentation/web-api/concepts/authorization
-const token =
-	"BQBbGTrvWZcQKSR9STspmHGFGXRUkUbi6URpvAiCwwEkA-O_oUeFBj12QVEBf0RWM06SNdBWQukUKDeCYCaS4b-bkSouri-Sky7FUstXxwHoLDnHA-k16aNenI_nDVTuzZ9gVdVLLfjhLcGbJjnnCf2klv9xTMYXR_782u5KSVr-lyAIytbBtpFXhPKIKkXIJl048lCbk7fK83n1373kNCtsKthI_VRFjIbyEWftNYJDyb-cNDmoaXZmgcewQo3CPVZsGKPqo_4MUgnWuwNnqUcwzJHo9bWKD6r-z-2TKg"
+const token = sessionStorage.getItem("xtoken")
 async function fetchWebApi(endpoint, method, body) {
 	const res = await fetch(`https://api.spotify.com/${endpoint}`, {
 		headers: {
