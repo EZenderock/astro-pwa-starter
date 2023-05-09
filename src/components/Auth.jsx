ff27622fe3707b3af6edbd71afbf19bcf1d31a19
EZenderock <zenderockemmanuel@gmail.com>
2023-05-09 12:32:46 +0100

    Update Auth.jsx

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,20 +1,7 @@
-import { useEffect, useState } from "react";
-
 export default function AuthComponent() {
-	const [show, setShow] = useState(false)
-	useEffect(() => {
-		const token = sessionStorage.getItem("xtoken")
-		if (token) {
-			setTimeout(() => {
-				window.location.href = "/"
-			}, 1000)
-		}
-
-	}, [])
-
 
 	return (
-		<div className={`absolute z-50 top-0 left-0 h-full w-full bg-zinc-900/60 backdrop-blur-2xl transition-all ${show ? 'scale-100 inline-block' : 'scale-0 hidden'}`}>
+		<div className={`absolute -z-50 top-0 left-0 h-full w-full bg-zinc-900/60 backdrop-blur-2xl transition-all `}>
 
 		</div>
 	)
